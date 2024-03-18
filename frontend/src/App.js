175e546312199a203eb36ff7fc2d375a580ada32
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-03-18 21:04:57 +0200

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>Movie Library App</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
