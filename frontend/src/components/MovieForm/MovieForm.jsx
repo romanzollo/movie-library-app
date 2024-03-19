793ea05e78444bce0c8c24d7a75214e273d5e90e
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-03-19 15:29:41 +0200

    Added movies to MovieList component by form

--- frontend/src/components/MovieForm/MovieForm.jsx
+++ frontend/src/components/MovieForm/MovieForm.jsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addMovie } from '../../redux/Movies/actionCreators';
+import { v4 as uuidv4 } from 'uuid';
+
 import './MovieForm.css';
 
 const MovieForm = () => {
+    const [title, setTitle] = useState('');
+    const [director, setDirector] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (title && director) {
+            const newMovie = { title, director, id: uuidv4() };
+            dispatch(addMovie(newMovie));
+
+            setTitle('');
+            setDirector('');
+        }
+    };
     return (
-        <div className="app-block block-form">
-            <h2>Movie Form</h2>
+        <div className="app-block movie-form">
+            <h2>Add a New Movie</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        id="title"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="director">Director:</label>
+                    <input
+                        type="text"
+                        value={director}
+                        onChange={(e) => setDirector(e.target.value)}
+                        id="director"
+                    />
+                </div>
+                <button type="submit">Add Movie</button>
+            </form>
         </div>
     );
 };
--- frontend/src/components/MovieList/MovieList.jsx
+++ frontend/src/components/MovieList/MovieList.jsx
@@ -1,9 +1,26 @@
+import { useSelector } from 'react-redux';
 import './MovieList.css';
 
 const MovieList = () => {
+    const movies = useSelector((state) => state.movies);
+
     return (
-        <div className="app-block block-list">
+        <div className="app-block movie-list">
             <h2>Movie List</h2>
+            {movies.length === 0 ? (
+                <p>No movies found</p>
+            ) : (
+                <ul>
+                    {movies.map((movie, i) => (
+                        <li key={movie.id}>
+                            <div className="movie-info">
+                                {++i}. {movie.title} by{' '}
+                                <strong>{movie.director}</strong>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
