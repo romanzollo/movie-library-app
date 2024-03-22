fcfa9ebde59248dadec649f3be0767abee264511
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-03-22 16:06:42 +0200

    Added errorSlice

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import movieReducer from './slices/movieSlice';
 import filterReducer from './slices/filterSlice';
+import errorReducer from './slices/errorSlice';
 
 const store = configureStore({
     reducer: {
         movies: movieReducer,
         filter: filterReducer,
+        error: errorReducer,
     },
 });
 
